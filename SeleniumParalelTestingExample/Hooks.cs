3f3ca2625780b13e033e1c47da472b34cdd4ab9f
sevilayagil <sevilayagil@gmail.com>
2019-12-17 18:11:24 +0300

    packages folder added to gitignore

---
+++ SeleniumParalelTestingExample/Hooks.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumParalelTestingExample
+{
+    [TestFixture]
+     public class Hooks:Base
+    {
+        public Hooks()
+        {
+            Driver = new FirefoxDriver();
+        }
+    }
+}
--- SeleniumParalelTestingExample/UnitTest1.cs
+++ SeleniumParalelTestingExample/UnitTest1.cs
@@ -1,25 +1,34 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace SeleniumParalelTestingExample
 {
     [TestFixture]
-    public class FirefoxTesting
+    public class FirefoxTesting :Hooks
     {
-        [TestMethod]
+        [Test]
         public void FirefoxGoogleTest()
         {
-
+            Driver.Navigate().GoToUrl("http://www.google.com"); 
+            Driver.FindElement(By.Name("q")).SendKeys("Selenium");
+            Driver.FindElement(By.Name("btnG")).Click();
+            Assert.That(Driver.PageSource.Contains("Selenium"),Is.EqualTo(true),
+                "The text selenium doesnt exist");
         }
     }
     [TestFixture]
-    public class ChromeTesting
+    public class ChromeTesting:Hooks
     {
-        [TestMethod]
+        [Test]
         public void ChromeGoogleTest()
         {
-
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("sevilay");
+            Driver.FindElement(By.Name("btnG")).Click();
+            Assert.That(Driver.PageSource.Contains("sevilay"), Is.EqualTo(true),
+                "The text selenium doesnt exist");
         }
     }
 }
